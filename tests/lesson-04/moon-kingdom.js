974377509eaae5cac6eef3eda058f0cc87e1e7ae
test01-ThuyHienLe <lethuyhien1309@gmail.com>
2026-06-16 22:43:01 +0700

    feat/moon-kingdom

--- tests/lesson-04/moon-kingdom.js
+++ tests/lesson-04/moon-kingdom.js
@@ -1,5 +1,5 @@
 //Bai 1
-
+//Cach 1
 function createCharacters() {
     let characters = [
         { name: 'hien', level: 4, health: 1200 },
@@ -23,8 +23,25 @@ function createCharacters() {
     console.log("Sau khi loc", possibleWinners);
 }
 createCharacters();
+//cach 2
+let charactersPowerUp = [];
+let characters = [
+    { name: 'hien', level: 4, health: 1200 },
+    { name: 'mai', level: 1, health: 100 },
+    { name: 'hoa', level: 5, health: 1500 },
+];
 
+for (let character of characters) {
+    charactersPowerUp.push({
+        name: character.name.toUpperCase(),
+        level: character.level * 2,
+        health: character.health * 3
+    });
+}
+createCharacters();
 //Bai 2
+
+//cach so 1
 function printLeaderboard() {
     let player = [
         { name: "Mario", score: 1000 },
@@ -39,15 +56,37 @@ function printLeaderboard() {
     for (i = 0; i <= 2; i++) {
         let huychuong = "";
         if (i === 0) {
-            console.log(huychuong = "🥇")}
+            console.log(huychuong = "🥇")
+        }
         else if (i === 1) {
             console.log(huychuong = "🥈")
         }
-        else if (i === 2){
+        else if (i === 2) {
             console.log(huychuong = "🥉")
         };
-        console.log((i+1) + ". " + player[i].name + " - " + player[i].score + " pts");
+        console.log((i + 1) + ". " + player[i].name + " - " + player[i].score + " pts");
     }
 
 }
 printLeaderboard();
+
+//cach so 2
+function printLeaderboard() {
+    let player = [
+        { name: "Mario", score: 1000 },
+        { name: "Luigi", score: 9000 },
+        { name: "Peach", score: 850 },
+        { name: "Yoshi", score: 7000 },
+        { name: "hien", score: 10000 },
+
+    ]
+    player.sort((a, b) => b.score - a.score);
+    console.log('Sau khi sap xep mang:', player);
+    console.log("🥇 " + player[0].name + " - " + player[0].score + " pts");
+    console.log("🥈 " + player[1].name + " - " + player[1].score + " pts");
+    console.log("🥉 " + player[2].name + " - " + player[2].score + " pts");
+
+}
+printLeaderboard();
+
+
