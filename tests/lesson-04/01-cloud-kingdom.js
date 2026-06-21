182e1de12e870cc9e9bd61b128c3ec2bf010fedc
test01-ThuyHienLe <lethuyhien1309@gmail.com>
2026-06-21 14:20:50 +0700

    fix: add solution for challenge 03

---
+++ tests/lesson-04/01-cloud-kingdom.js
@@ -0,0 +1,17 @@
+//Cách số 1
+
+let powerUp = 'mushroom';
+if (powerUp === 'mushroom') {
+    console.log('Mario becomes Super!');
+}
+else if (powerUp === 'flower') {
+    console.log('Mario can shoot fireballs!');
+}
+else if (powerUp === 'star') {
+    console.log('Mario is invincible!');
+}
+else if (powerUp === 'none') {
+console.log("Mario is normal");
+} else {
+console.log("Unknown power-up");
+}
